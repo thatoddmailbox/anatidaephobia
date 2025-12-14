ff655dfddd7143bbc9983dbd67d7997d08cd6e05
Alex Studer <alex@studer.dev>
2025-12-14 15:15:16 -0500

    add fabric template

---
+++ src/client/java/dev/studer/alex/anatidaephobia/AnatidaephobiaClient.java
@@ -0,0 +1,10 @@
+package dev.studer.alex.anatidaephobia;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AnatidaephobiaClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
