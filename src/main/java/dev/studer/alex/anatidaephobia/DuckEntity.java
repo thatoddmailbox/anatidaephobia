473fb19a4a20f15d45d95fff0c14112e97b920af
Alex Studer <alex@studer.dev>
2025-12-14 20:17:37 -0500

    DuckEntity: give some goals

--- src/main/java/dev/studer/alex/anatidaephobia/DuckEntity.java
+++ src/main/java/dev/studer/alex/anatidaephobia/DuckEntity.java
@@ -2,10 +2,33 @@ package dev.studer.alex.anatidaephobia;
 
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.PathfinderMob;
+import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
+import net.minecraft.world.entity.ai.attributes.Attributes;
+import net.minecraft.world.entity.ai.goal.FloatGoal;
+import net.minecraft.world.entity.ai.goal.LookAtPlayerGoal;
+import net.minecraft.world.entity.ai.goal.WaterAvoidingRandomStrollGoal;
+import net.minecraft.world.entity.animal.Animal;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
 
 public class DuckEntity extends PathfinderMob {
 	public DuckEntity(EntityType<? extends PathfinderMob> entityType, Level level) {
 		super(entityType, level);
+	}
+
+	@Override
+	protected void registerGoals() {
+		this.goalSelector.addGoal(0, new FloatGoal(this));
+		this.goalSelector.addGoal(1, new WaterAvoidingRandomStrollGoal(this, 1.0));
+//		this.goalSelector.addGoal(2, new LookAtPlayerGoal(this, Player.class, 6.0F));
+	}
+
+	public static AttributeSupplier.Builder createMobAttributes() {
+		return Animal.createAnimalAttributes()
+				.add(Attributes.MOVEMENT_SPEED, 0.25F)
+				.add(Attributes.MAX_HEALTH, 10.0)
+				.add(Attributes.ATTACK_DAMAGE, 2.0)
+				.add(Attributes.SAFE_FALL_DISTANCE, 5.0)
+				.add(Attributes.FOLLOW_RANGE, 32.0);
 	}
 }
