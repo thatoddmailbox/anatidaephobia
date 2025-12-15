735909a87015129f626bc6e5118b06c5adafd7f3
Alex Studer <alex@studer.dev>
2025-12-14 20:10:20 -0500

    fix indents

--- src/main/java/dev/studer/alex/anatidaephobia/AnatidaephobiaEntities.java
+++ src/main/java/dev/studer/alex/anatidaephobia/AnatidaephobiaEntities.java
@@ -10,17 +10,17 @@ import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.MobCategory;
 
 public class AnatidaephobiaEntities {
-    public static final Identifier DUCK_ID = Identifier.tryBuild(Anatidaephobia.MOD_ID, "duck");
-    public static final EntityType<DuckEntity> DUCK = Registry.register(
-            BuiltInRegistries.ENTITY_TYPE,
-            DUCK_ID,
-            EntityType.Builder.of(DuckEntity::new, MobCategory.CREATURE).sized(1, 1).build(ResourceKey.create(
-                    Registries.ENTITY_TYPE,
-                    DUCK_ID
-            ))
-    );
+	public static final Identifier DUCK_ID = Identifier.tryBuild(Anatidaephobia.MOD_ID, "duck");
+	public static final EntityType<DuckEntity> DUCK = Registry.register(
+			BuiltInRegistries.ENTITY_TYPE,
+			DUCK_ID,
+			EntityType.Builder.of(DuckEntity::new, MobCategory.CREATURE).sized(1, 1).build(ResourceKey.create(
+					Registries.ENTITY_TYPE,
+					DUCK_ID
+			))
+	);
 
-    public static void init() {
-        FabricDefaultAttributeRegistry.register(DUCK, DuckEntity.createMobAttributes());
-    }
+	public static void init() {
+		FabricDefaultAttributeRegistry.register(DUCK, DuckEntity.createMobAttributes());
+	}
 }
--- src/main/java/dev/studer/alex/anatidaephobia/DuckEntity.java
+++ src/main/java/dev/studer/alex/anatidaephobia/DuckEntity.java
@@ -5,7 +5,7 @@ import net.minecraft.world.entity.PathfinderMob;
 import net.minecraft.world.level.Level;
 
 public class DuckEntity extends PathfinderMob {
-    public DuckEntity(EntityType<? extends PathfinderMob> entityType, Level level) {
-        super(entityType, level);
-    }
+	public DuckEntity(EntityType<? extends PathfinderMob> entityType, Level level) {
+		super(entityType, level);
+	}
 }
