18c380ca96e81dd6e96adaa1512b6a8de85b7742
Alex Studer <alex@studer.dev>
2025-12-14 22:11:49 -0500

    DuckEntity: allow ducks to eat bread (but nothing happens yet)

--- src/main/java/dev/studer/alex/anatidaephobia/DuckEntity.java
+++ src/main/java/dev/studer/alex/anatidaephobia/DuckEntity.java
@@ -1,8 +1,11 @@
 package dev.studer.alex.anatidaephobia;
 
+import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.network.chat.Component;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.EntitySpawnReason;
 import net.minecraft.world.entity.EntityType;
@@ -15,9 +18,14 @@ import net.minecraft.world.entity.ai.goal.LookAtPlayerGoal;
 import net.minecraft.world.entity.ai.goal.TemptGoal;
 import net.minecraft.world.entity.ai.goal.WaterAvoidingRandomStrollGoal;
 import net.minecraft.world.entity.animal.Animal;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
 import net.minecraft.world.level.Level;
 
 public class DuckEntity extends PathfinderMob {
+	private byte EVENT_ID_LOVE = 100;
+
 	public DuckEntity(EntityType<? extends PathfinderMob> entityType, Level level) {
 		super(entityType, level);
 	}
@@ -28,6 +36,42 @@ public class DuckEntity extends PathfinderMob {
 		this.goalSelector.addGoal(1, new TemptGoal(this, 1.0, itemStack -> itemStack.is(AnatidaephobiaItems.DUCK_FOOD), false));
 		this.goalSelector.addGoal(2, new WaterAvoidingRandomStrollGoal(this, 1.0));
 //		this.goalSelector.addGoal(2, new LookAtPlayerGoal(this, Player.class, 6.0F));
+	}
+
+	@Override
+	public void handleEntityEvent(final byte id) {
+		if (id == EVENT_ID_LOVE) {
+			for(int i = 0; i < 7; ++i) {
+				double xa = this.random.nextGaussian() * 0.02;
+				double ya = this.random.nextGaussian() * 0.02;
+				double za = this.random.nextGaussian() * 0.02;
+				this.level().addParticle(ParticleTypes.HEART, this.getRandomX((double)1.0F), this.getRandomY() + (double)0.5F, this.getRandomZ((double)1.0F), xa, ya, za);
+			}
+		} else {
+			super.handleEntityEvent(id);
+		}
+	}
+
+	@Override
+	public InteractionResult mobInteract(final Player player, final InteractionHand hand) {
+		ItemStack itemStack = player.getItemInHand(hand);
+		if (itemStack.is(Items.BREAD)) {
+			if (player instanceof ServerPlayer) {
+				ServerPlayer serverPlayer = (ServerPlayer) player;
+				serverPlayer.sendSystemMessage(Component.translatable("message.anatidaephobia.duck_hurt"));
+
+				this.usePlayerItem(player, hand, itemStack);
+				this.level().broadcastEntityEvent(this, EVENT_ID_LOVE);
+
+				return InteractionResult.SUCCESS_SERVER;
+			}
+
+			if (this.level().isClientSide()) {
+				return InteractionResult.CONSUME;
+			}
+		}
+
+		return super.mobInteract(player, hand);
 	}
 
 	@Override
